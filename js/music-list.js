5d67d287263e69f95fc3e2c3f5161dfe9cef4442
Itz_Gabby <oshowobigabriel02@gmail.com>
2024-12-20 13:20:26 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Fido - Awolowo",
+        artist: "Fido",
+        img: "Awolowo",
+        src: "Awolowo"
+    },
+    {
+        name: "Asake - Fuji Vibes",
+        artist: "Asake",
+        img: "Asake",
+        src: "Asake"
+    },
+    {
+        name: "Davido & YG Marley - Awuke",
+        artist: "Davido & YG Marley",
+        img: "Awuke",
+        src: "Awuke"
+    },
+    {
+        name: "Ayo Maff - Are You There?",
+        artist: "Ayo Maff",
+        img: "Maff",
+        src: "Maff"
+    },
+    {
+        name: "Asake & Wizkid - MMS",
+        artist: "Asake & Wizkid",
+        img: "Asake",
+        src: "As"
+    },
+    {
+        name: "Buju & Rema - Fi Kan We kan ",
+        artist: "Buju & Rema",
+        img: "Bnxn",
+        src: "Bnxn"
+    },
+]
