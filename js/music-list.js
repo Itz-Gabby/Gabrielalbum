4caf681b502c4f1a8f4dc05a8ce5bdb34aa4ac6b
Itz_Gabby <oshowobigabriel02@gmail.com>
2025-03-13 19:39:42 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic = [ 
+    {
+        name: "Fido - Awolowo",
+        artist: "Fido",
+        img: "Awolowo",
+        src: "Awolowo"
+    },
+    {
+        name: "Asake - Fuji Vibes",
+        artist: "Asake",
+        img: "Asake",
+        src: "Asake"
+    },
+    {
+        name: "Tope Alabi - You Are Worthy ",
+        artist: "Tope Alabi",
+        img: "Tope",
+        src: "Tope"
+    },
+    {
+        name: "Fido - Joy Is Coming",
+        artist: "Fido",
+        img: "Fido",
+        src: "Fido"
+    },
+    {
+        name: "Asake & Wizkid - MMS",
+        artist: "Asake & Wizkid",
+        img: "Asake",
+        src: "As"
+    },
+    {
+        name: "Sola Allyson - Imisi ",
+        artist: "Sola Allyson",
+        img: "Sola",
+        src: "Sola"
+    },
+    {
+        name: "Teni - Baby(Is It A Crime) ",
+        artist: "Rema",
+        img: "Baby",
+        src: "Rema"
+    },
+    {
+        name: "Asake - Uhh Yeah ",
+        artist: "Asake",
+        img: "Asake",
+        src: "Uhh"
+    },
+    {
+        name: "Buju & Rema - Fi Kan We kan ",
+        artist: "Buju & Rema",
+        img: "Bnxn",
+        src: "Bnxn"
+    },
+    {
+        name: "Ayo Maff - Are you there? ",
+        artist: "Ayo Maff",
+        img: "Ayo",
+        src: "Ayo"
+    },
+    {
+        name: "Teni - Money ",
+        artist: "Teni",
+        img: "TENI",
+        src: "TENI"
+    },
+    {
+        name: "Joeblaq & Zinolesky - Jo Vibe ",
+        artist: "Joeblaq & Zinolesky",
+        img: "Joe",
+        src: "Joe"
+    },
+    {
+        name: "Asake - Why Love ",
+        artist: "Asake",
+        img: "Why",
+        src: "Why"
+    },
+]
